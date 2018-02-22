03031986153de5d4dab9f2444d16e0c002a309e2
Alumno <Alumno@W7Bsica>
2018-02-22 13:38:27 +0100

    Ya funciona la ordenacion, queda pendiente la busqueda

--- U5P02-Java-Catalogo/src/servlet/MostrarCatalogoServlet.java
+++ U5P02-Java-Catalogo/src/servlet/MostrarCatalogoServlet.java
@@ -38,68 +38,25 @@ public class MostrarCatalogoServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		Cancion cancion;
-		String order = "nombre"; // Se ordena por nombre de forma predeterminada
-
-		ServletContext contexto = getServletContext();
-		response.setContentType("text/html;UTF-8");
 		PrintWriter out = response.getWriter();
-		Connection conn = null;
-		Statement sentencia = null;
-
-		// Botones de arriba y abajo deshabilitados si es la opcion elegida
-
-		String naz = "href= MostrarCatalogo?res=NAZ";
-		String nza = "href= MostrarCatalogo?res=NZA";
-		String aaz = "href= MostrarCatalogo?res=AAZ";
-		String aza = "href= MostrarCatalogo?res=AZA";
-		String alaz = "href= MostrarCatalogo?res=ALAZ";
-		String alza = "href= MostrarCatalogo?res=ALZA";
-
-		if (!request.getParameterMap().containsKey("res") || request.getParameter("res").equals("NAZ")) {
-			order = "nombre";
-			naz = "";
-		}
-
-		if (request.getParameterMap().containsKey("res")) {
-
-			if (request.getParameter("res").equals("NZA")) {
-				order = "nombre desc";
-				nza = "";
-			}
-			if (request.getParameter("res").equals("AAZ")) {
-				order = "autor desc";
-				aaz = "";
-			}
-			if (request.getParameter("res").equals("AZA")) {
-				order = "autor asc";
-				aza = "";
-			}
-			if (request.getParameter("res").equals("ALZA")) {
-				order = "album desc";
-				alza = "";
-			}
-			if (request.getParameter("res").equals("ALAZ")) {
-				order = "album asc";
-				alaz = "";
-			}
-
-		}
-		//////////////////////////////////////////////////
-		// footer
-		out.println("<style type='text/css'>");
-		out.println("footer {");
-		out.println("position: fixed;");
-		out.println("right: 0;");
-		out.println("bottom: 0;");
-		out.println("left: 0;");
-		out.println("padding: 1rem;");
-		out.println("background-color: #efefef;");
-		out.println("text-align: center;");
-		out.println("}</style>");
-		///////
 
 		try {
+			Cancion cancion;
+			
+			//Variables para pasar parametros, (sin una sesion)
+			String aut = "";
+			String res = "";
+			String can = "";
+			////
+			String filtro = "";
+			String busqueda = "";
+			String order = "nombre"; // Se ordena por nombre de forma predeterminada
+
+			ServletContext contexto = getServletContext();
+			response.setContentType("text/html;UTF-8");
+			Connection conn = null;
+			Statement sentencia = null;
+
 			// Paso 1: Cargar el driver JDBC.
 			Class.forName("org.mariadb.jdbc.Driver").newInstance();
 			// Paso 2: Conectarse a la Base de Datos utilizando la clase Connection
@@ -109,9 +66,10 @@ public class MostrarCatalogoServlet extends HttpServlet {
 			conn = DriverManager.getConnection(url, userName, password);
 			// Paso 3: Crear sentencias SQL, utilizando objetos de tipo Statement
 			sentencia = conn.createStatement();
-			String filtro = "";
-			////////////////////////////////// 77
-			String busqueda = "";
+	
+
+			////////////////////////////////// 
+
 			// Autor
 
 			if (request.getParameterMap().containsKey("aut")) {
@@ -128,8 +86,64 @@ public class MostrarCatalogoServlet extends HttpServlet {
 			ResultSet rset = sentencia.executeQuery(consulta);
 			String consulta2;
 
+			// Botones de arriba y abajo deshabilitados si es la opcion elegida
+
+			String naz = "href= MostrarCatalogo?res=NAZ";
+			String nza = "href= MostrarCatalogo?res=NZA";
+			String aaz = "href= MostrarCatalogo?res=AAZ";
+			String aza = "href= MostrarCatalogo?res=AZA";
+			String alaz = "href= MostrarCatalogo?res=ALAZ";
+			String alza = "href= MostrarCatalogo?res=ALZA";
+
+			if (!request.getParameterMap().containsKey("res") || request.getParameter("res").equals("NAZ")) {
+				order = "nombre";
+				naz = "";
+			}
+
+			if (request.getParameterMap().containsKey("res")) {
+
+				res = "&res=" + request.getParameter("res");
+				if (request.getParameter("res").equals("NZA")) {
+					order = "nombre desc";
+					nza = "";
+				}
+				if (request.getParameter("res").equals("AAZ")) {
+					order = "autor desc";
+					aaz = "";
+				}
+				if (request.getParameter("res").equals("AZA")) {
+					order = "autor asc";
+					aza = "";
+				}
+				if (request.getParameter("res").equals("ALZA")) {
+					order = "album desc";
+					alza = "";
+				}
+				if (request.getParameter("res").equals("ALAZ")) {
+					order = "album asc";
+					alaz = "";
+				}
+
+			}
+
+			//////////////////////////////////////////////////
+
+			// Autor
+			if (request.getParameterMap().containsKey("aut")) {
+				aut = "&aut=" + request.getParameter("aut");
+				consulta2 = "SELECT * FROM autores where ID_AUTOR=" + request.getParameter("aut");
+				ResultSet rset2 = sentencia.executeQuery(consulta2);
+				if (!rset2.isBeforeFirst()) {
+					out.println("<p>No hay autores en la base de datos</p>");
+				}
+				if (rset2.next())
+					out.println("<div><img src=./img/" + rset2.getString("FOTO") + " height=150 /></div>");
+			}
+			////////////////////////////// 77
+
 			// Busqueda
 			if (request.getParameterMap().containsKey("can")) {
+				can = "&can=" + request.getParameter("can");
 				String[] split;
 				split = request.getParameter("can").split(" ");
 
@@ -142,6 +156,20 @@ public class MostrarCatalogoServlet extends HttpServlet {
 				}
 			}
 
+			/////////////
+			// footer
+			out.println("<style type='text/css'>");
+			out.println("footer {");
+			out.println("position: fixed;");
+			out.println("right: 0;");
+			out.println("bottom: 0;");
+			out.println("left: 0;");
+			out.println("padding: 1rem;");
+			out.println("background-color: #efefef;");
+			out.println("text-align: center;");
+			out.println("}</style>");
+			///////
+
 			// Resultado
 			out.println("<html><head><meta charset='UTF-8'/></head><body>");
 
@@ -149,14 +177,14 @@ public class MostrarCatalogoServlet extends HttpServlet {
 			out.println("<table style='border: 0'>");
 			out.println("<tr style='background-color: whitesmoke'>");
 
-			out.println("<th>Nombre <a " + naz + ">&#9652</a>"); // Nombre AZ
-			out.println("<a " + nza + ">&#9662</a></th>"); // Nombre ZA
+			out.println("<th>Nombre <a " + naz + aut + can + ">&#9652</a>"); // Nombre AZ
+			out.println("<a " + nza + aut + can + ">&#9662</a></th>"); // Nombre ZA
 
-			out.println("<th>ALBUM <a " + alaz + ">&#9652</a>"); // Album AZ
-			out.println("<a " + alza + ">&#9662</a></th>"); // Album ZA
+			out.println("<th>ALBUM <a " + alaz + aut + can + ">&#9652</a>"); // Album AZ
+			out.println("<a " + alza + aut + can + ">&#9662</a></th>"); // Album ZA
 
-			out.println("<th>AUTOR <a " + aaz + " >&#9652</a>"); // Autor AZ
-			out.println("<a " + aza + " >&#9662</a></th>"); // Autor ZA
+			out.println("<th>AUTOR <a " + aaz + aut + can + " >&#9652</a>"); // Autor AZ
+			out.println("<a " + aza + aut + can + " >&#9662</a></th>"); // Autor ZA
 
 			out.println("</tr>");
 			/////////////////////////////////
@@ -172,20 +200,8 @@ public class MostrarCatalogoServlet extends HttpServlet {
 
 				out.print("<tr><td><a href=./MostrarObra?id=" + cancion.getID_CANCION() + ">" + cancion.getNOMBRE()
 						+ "</a></td><td>" + rset.getString("album") + "</td><td><a href=./MostrarCatalogo?aut="
-						+ cancion.getID_AUTOR() + ">" + rset.getString("autor") + "</a></td></tr>");
+						+ cancion.getID_AUTOR() + can + res + ">" + rset.getString("autor") + "</a></td></tr>");
 			}
-
-			// Autor
-			if (request.getParameterMap().containsKey("aut")) {
-				consulta2 = "SELECT * FROM autores where ID_AUTOR=" + request.getParameter("aut");
-				ResultSet rset2 = sentencia.executeQuery(consulta2);
-				if (!rset2.isBeforeFirst()) {
-					out.println("<p>No hay autores en la base de datos</p>");
-				}
-				if (rset2.next())
-					out.println("<div><img src=./img/" + rset2.getString("FOTO") + " height=150 /></div>");
-			}
-			////////////////////////////// 77
 
 			// Quitar filtros.
 			if (request.getParameterMap().containsKey("filtro")) {
@@ -197,10 +213,9 @@ public class MostrarCatalogoServlet extends HttpServlet {
 			///////////////
 			// Navegacion
 
-			out.println("<footer> <p><form action=./MostrarCatalogo method=post>"
+			out.println("<footer> <p><form action=" + contexto.getContextPath() + res + aut + " method=post>"
 					+ "<label>Introduce el nombre de la cancion a buscar..</label><input type=text name=can >"
-					+ "<input type=submit></form></p>"
-					+ "<p><a href=./U5P01-Java-BBDD/MostrarCuidadores>Mostrar Cuidadores</a></p>");
+					+ "<input type=submit></form></p>");
 			out.println("<a href=./MostrarCatalogo?filtro=0 >Eliminar filtros.</a>");
 			out.println("</footer>");
 			out.println("</body></html>");
